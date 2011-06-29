c9e387443a78921cce9053f2748ebb70a78a58e6
Jim Morris <morris@wolfman.com>
2011-06-29 02:36:44 -0700

    tweaked the logging config file

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -5,6 +5,8 @@ import ch.qos.logback.core.ConsoleAppender
 import static ch.qos.logback.classic.Level.INFO
 import static ch.qos.logback.classic.Level.TRACE
 import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.ERROR
+import static ch.qos.logback.classic.Level.WARN
 
 appender("RootConsoleAppender", ConsoleAppender) {
   filter(ThresholdFilter) {
@@ -14,4 +16,11 @@ appender("RootConsoleAppender", ConsoleAppender) {
 	pattern = "%r [%t] %p, %c - %m%n"
   }
 }
-root(TRACE, ["RootConsoleAppender"])
+
+root(INFO, ["RootConsoleAppender"])
+
+logger("com.e4net", DEBUG)
+logger("httpclient.wire.content", WARN)
+logger("httpclient.wire.header", WARN)
+logger("org.apache.commons.httpclient", WARN)
+logger("org.apache.commons.httpclient.HttpMethodDirector", ERROR)
